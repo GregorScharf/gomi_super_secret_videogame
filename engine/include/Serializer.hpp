b86cb6ee32827cd8e6baf00bfde0cd0e0743f264
greg <aether@pop-os.fritz.box>
2024-11-24 17:42:17 +0100

    stupid branching mistake

--- engine/include/Serializer.hpp
+++ engine/include/Serializer.hpp
@@ -1,5 +1,7 @@
 #pragma once
+#include "containers.hpp"
 #include "types.hpp"
+
 
 class Serializer {
 private:
@@ -11,8 +13,10 @@ public:
     serializer_missing_inputfiles = -1,
     serializer_ok = 0,
     serializer_ok_duplicates = 1,
-    //serializer_ok_
+    // serializer_ok_
   };
-  static enum SerializerCode ToToml(GameObjectContainer& g_obj_cont, IconContainer& icon_cont /*, &ShaderIcon shade_cont*/);
+  static enum SerializerCode
+  ToToml(GameObjectContainer &g_obj_cont,
+         IconContainer &icon_cont /*, &ShaderIcon shade_cont*/);
   enum SerializerCode FromToml(void);
 };
