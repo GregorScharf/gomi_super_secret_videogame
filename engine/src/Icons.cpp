87a298469763f10cc5de9e168d158372cf18f220
byron39 <>
2024-11-28 09:51:39 -0500

    serializer to toml initial file output

--- engine/src/Icons.cpp
+++ engine/src/Icons.cpp
@@ -14,10 +14,27 @@ TextureIcon::TextureIcon(string path, i32 x, i32 y) {
   text = path.substr(path.find_last_of('/') + 1,
                      path.size() - path.find_last_of('/') - 1);
   fontsize = 18;
+<<<<<<< HEAD
   BorderThickness = 3;
   // this is kind of a hack but ehhh
   i32 charsPerLine = scale.width / fontsize;
   charsPerLine--;
+=======
+  border_thickness = 3;
+}
+
+void TextureIcon::make_ref(node<TextureIcon *> *reference) { ref = reference; }
+
+void TextureIcon::draw(Rectangle *barFrame) {
+
+  // this is kind of a hack but ehhh
+  i32 charsPerLine = scale.width / fontsize;
+  charsPerLine--;
+  Rectangle dest = {scale.x, scale.y + (barFrame->height * 2),
+                    f32((BOX_WIDTH)-border_thickness),
+                    f32((scale.height) - 2 * border_thickness)};
+
+>>>>>>> a304625 (serializer to toml initial file output)
   i32 lines = text.size() / charsPerLine;
 
   string drawText = text;
@@ -25,6 +42,7 @@ TextureIcon::TextureIcon(string path, i32 x, i32 y) {
   for (i32 i = 0; i < lines; i++) {
     drawText.insert((i + 1) * charsPerLine, "\n");
   }
+<<<<<<< HEAD
   text = drawText;
 }
 
@@ -36,13 +54,16 @@ void TextureIcon::draw(Rectangle *barFrame) {
                     f32((scale.height) - 2 * BorderThickness)};
 
   DrawRectangleLinesEx(dest, BorderThickness, Color{30, 30, 30, 255});
+=======
+  DrawRectangleLinesEx(dest, border_thickness, Color{30, 30, 30, 255});
+>>>>>>> a304625 (serializer to toml initial file output)
 
   DrawTextEx(Fonts::Carlito, text.c_str(), {dest.x + 10, dest.y + 3}, 18, 1,
              BLACK);
 
   DrawTexturePro(texture, {0, 0, (f32)texture.width, (f32)texture.height},
-                 {dest.x + (f32)BOX_WIDTH / 2, dest.y + BorderThickness,
-                  (f32)(BOX_WIDTH / 2) - BorderThickness * 2,
-                  dest.height - 2 * BorderThickness},
+                 {dest.x + (f32)BOX_WIDTH / 2, dest.y + border_thickness,
+                  (f32)(BOX_WIDTH / 2) - border_thickness * 2,
+                  dest.height - 2 * border_thickness},
                  {0, 0}, 0, RAYWHITE);
 }
